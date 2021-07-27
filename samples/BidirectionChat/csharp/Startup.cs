ac9c07b1ca6749f3f338ef00b1837b2d16907215
Bramley Turner-Jones <bramley@jetcharge.com.au>
2021-07-27 18:17:24 +1000

    Minimum reproducable changes to cause 'Azure Functions runtime is unreachable'

---
+++ samples/BidirectionChat/csharp/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+//using Microsoft.EntityFrameworkCore;
+
+
+[assembly: FunctionsStartup(typeof(bidirectional_chat.Startup))]
+namespace bidirectional_chat
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            Console.WriteLine("App Startup...");
+            var executionContextOptions = builder.Services.BuildServiceProvider().GetService<IOptions<ExecutionContextOptions>>().Value;
+            var appDirectory = executionContextOptions.AppDirectory;
+            //Used by SignalR
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc
+            };
+            //Used by rest of the API
+            //Comment out this block to resolve the "Azure Functions runtime is unreachable" error:
+            builder.Services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+
+            });
+
+        }
+    }
+}
